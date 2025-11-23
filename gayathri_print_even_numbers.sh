4bf35d7f89a24aa516abedbb1a921c9dad8e9c4a
gayathritheju1812-eng <gayathritheju1812@gmail.com>
2025-11-23 17:15:20 +0530

    added print even numbers file

---
+++ gayathri_print_even_numbers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#using for loop to check whether the number is even or odd
+
+
+num=$1
+
+for((i=0;i<=num;i++))
+do
+        if((i%2==0))
+        then
+                echo $i
+        fi
+done
+
+
